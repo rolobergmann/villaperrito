8a65409dafde2de4dc297ebffdf59fa1ad151194
rolobergmann <rolando.bergmann.p@gmail.com>
2024-01-30 12:56:44 -0300

    added carousel and instagram link

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -1,6 +1,10 @@
 let iconCart = document.querySelector(".icon-cart");
+let closeCart = document.querySelector(".close");
 let body = document.querySelector("body");
 
 iconCart.addEventListener("click", () => {
-  body.classList.toggle("showCart");
+	body.classList.toggle("showCart");
+});
+closeCart.addEventListener("click", () => {
+	body.classList.toggle("showCart");
 });
