852dc8007658cab3b8be0db00f3143be00b239b3
rolobergmann <rolando.bergmann.p@gmail.com>
2024-01-30 19:10:13 -0300

    Product list

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -1,6 +1,11 @@
+let listProductHTML = document.querySelector(".listProduct");
+let listCartHTML = document.querySelector(".listCart");
 let iconCart = document.querySelector(".icon-cart");
-let closeCart = document.querySelector(".close");
+let iconCartSpan = document.querySelector(".icon-cart span");
 let body = document.querySelector("body");
+let closeCart = document.querySelector(".close");
+let products = [];
+let cart = [];
 
 iconCart.addEventListener("click", () => {
 	body.classList.toggle("showCart");
@@ -8,3 +13,70 @@ iconCart.addEventListener("click", () => {
 closeCart.addEventListener("click", () => {
 	body.classList.toggle("showCart");
 });
+
+const addDataToHTML = () => {
+	if (products.length > 0) {
+		// if has data
+		products.forEach((product) => {
+			let newProduct = document.createElement("div");
+			newProduct.dataset.id = product.id;
+			newProduct.classList.add("item");
+			newProduct.classList.add("col-sm-12");
+			newProduct.classList.add("col-md-4");
+			newProduct.classList.add("justify-content-center");
+			newProduct.classList.add("mb-5");
+			newProduct.classList.add("d-flex");
+			newProduct.innerHTML = `
+			<div
+				class="card"
+				style="width: 18rem">
+				<img
+					src="${product.image}"
+					class="card-img-top"
+					alt="..." />
+				<div class="card-body">
+					<h5 class="card-title">${product.name}</h5>
+					<p class="card-text">
+						Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+						accusantium doloremque laudantium, totam rem aperiam, eaque
+						ipsa quae ab illo inventore veritatis et quasi architecto
+						beatae vitae dicta sunt explicabo..
+					</p>
+				</div>
+				<ul class="list-group list-group-flush">
+					<li class="list-group-item price">$${product.price}</li>
+				</ul>
+				<div class="card-body">
+					<button class="addCart">Agregar al carro</button>
+				</div>
+			
+		</div>`;
+			listProductHTML.appendChild(newProduct);
+			console.log(newProduct);
+			console.log(product);
+
+			/* `<img src="${product.image}" alt="">
+			<h2>${product.name}</h2>
+			<div class="price">$${product.price}</div>
+			<button class="addCart">Add To Cart</button>`;
+		listProductHTML.appendChild(newProduct); */
+		});
+	}
+};
+
+const initApp = () => {
+	// get data product
+	fetch("/json/productos.json")
+		.then((response) => response.json())
+		.then((data) => {
+			products = data;
+			addDataToHTML();
+
+			// get data cart from memory
+			if (localStorage.getItem("cart")) {
+				cart = JSON.parse(localStorage.getItem("cart"));
+				addCartToHTML();
+			}
+		});
+};
+initApp();
