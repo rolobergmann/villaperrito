e9b3fa32aedf31c9d705795842df33e1848973fc
rolobergmann <rolando.bergmann.p@gmail.com>
2024-02-04 02:52:29 -0300

    add to cart

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -26,6 +26,8 @@ const addDataToHTML = () => {
 			newProduct.classList.add("justify-content-center");
 			newProduct.classList.add("mb-5");
 			newProduct.classList.add("d-flex");
+			newProduct.dataset.id = product.id;
+
 			newProduct.innerHTML = `
 			<div
 				class="card"
@@ -52,16 +54,132 @@ const addDataToHTML = () => {
 			
 		</div>`;
 			listProductHTML.appendChild(newProduct);
-			console.log(newProduct);
-			console.log(product);
-
-			/* `<img src="${product.image}" alt="">
-			<h2>${product.name}</h2>
-			<div class="price">$${product.price}</div>
-			<button class="addCart">Add To Cart</button>`;
-		listProductHTML.appendChild(newProduct); */
 		});
 	}
+};
+
+listProductHTML.addEventListener("click", (event) => {
+	let positionClick = event.target;
+	if (positionClick.classList.contains("addCart")) {
+		let product_id =
+			positionClick.parentElement.parentElement.parentElement.dataset.id;
+		addToCart(product_id);
+	}
+});
+
+const sendCart = (listCart) => {
+	console.log(listCart);
+	if (cart.length === 0) {
+		alert("Carro vacio");
+	} else {
+		(window.location =
+			"https://wa.me/56996330572?text=hola, estas comprando lo siguiente:" +
+			cart),
+			"_blank";
+	}
+};
+
+const addToCart = (product_id) => {
+	let positionThisProductInCart = cart.findIndex(
+		(value) => value.product_id == product_id
+	);
+	if (cart.length <= 0) {
+		cart = [
+			{
+				product_id: product_id,
+				quantity: 1,
+			},
+		];
+	} else if (positionThisProductInCart < 0) {
+		cart.push({
+			product_id: product_id,
+			quantity: 1,
+		});
+	} else {
+		cart[positionThisProductInCart].quantity =
+			cart[positionThisProductInCart].quantity + 1;
+	}
+	addCartToHTML();
+	addCartToMemory();
+};
+
+const addCartToMemory = () => {
+	localStorage.setItem("cart", JSON.stringify(cart));
+};
+
+const addCartToHTML = () => {
+	listCartHTML.innerHTML = "";
+	let totalQuantity = 0;
+	if (cart.length > 0) {
+		cart.forEach((item) => {
+			totalQuantity = totalQuantity + item.quantity;
+			let newItem = document.createElement("div");
+			newItem.classList.add("item");
+			newItem.dataset.id = item.product_id;
+
+			let positionProduct = products.findIndex(
+				(value) => value.id == item.product_id
+			);
+			let info = products[positionProduct];
+			listCartHTML.appendChild(newItem);
+			newItem.innerHTML = `
+            <div class="image">
+                    <img src="${info.image}">
+                </div>
+                <div class="name">
+                ${info.name}
+                </div>
+                <div class="totalPrice">$${info.price * item.quantity}</div>
+                <div class="quantity">
+                    <span class="minus"><</span>
+                    <span>${item.quantity}</span>
+                    <span class="plus">></span>
+                </div>
+            `;
+		});
+	}
+	iconCartSpan.innerText = totalQuantity;
+};
+
+listCartHTML.addEventListener("click", (event) => {
+	let positionClick = event.target;
+	if (
+		positionClick.classList.contains("minus") ||
+		positionClick.classList.contains("plus")
+	) {
+		let product_id = positionClick.parentElement.parentElement.dataset.id;
+		let type = "minus";
+		if (positionClick.classList.contains("plus")) {
+			type = "plus";
+		}
+		changeQuantityCart(product_id, type);
+	}
+});
+
+const changeQuantityCart = (product_id, type) => {
+	let positionItemInCart = cart.findIndex(
+		(value) => value.product_id == product_id
+	);
+	if (positionItemInCart >= 0) {
+		let info = cart[positionItemInCart];
+		switch (type) {
+			case "plus":
+				cart[positionItemInCart].quantity =
+					cart[positionItemInCart].quantity + 1;
+				break;
+
+			default:
+				let changeQuantity = cart[positionItemInCart].quantity - 1;
+				if (changeQuantity > 0) {
+					cart[positionItemInCart].quantity = changeQuantity;
+				} else {
+					cart.splice(positionItemInCart, 1);
+				}
+				break;
+		}
+	}
+	addCartToHTML();
+	addCartToMemory();
 };
 
 const initApp = () => {
@@ -79,4 +197,5 @@ const initApp = () => {
 			}
 		});
 };
+
 initApp();
