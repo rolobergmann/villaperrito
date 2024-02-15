b5ea62a568ef0d7ee26a40a2ef72005cfe9db0d6
rolobergmann <rolando.bergmann.p@gmail.com>
2024-02-15 16:16:21 -0300

    new view and footer

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -59,10 +59,7 @@ const addDataToHTML = () => {
 				<div class="card-body">
 					<h5 class="card-title">${product.name}</h5>
 					<p class="card-text">
-						Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-						accusantium doloremque laudantium, totam rem aperiam, eaque
-						ipsa quae ab illo inventore veritatis et quasi architecto
-						beatae vitae dicta sunt explicabo..
+						${product.descripcion}
 					</p>
 				</div>
 				<ul class="list-group list-group-flush">
